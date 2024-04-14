1a6f54779e9fd895d89684e5b5e89dd9c773efb4
Jesus9498 <jmartinezmiranda@csumb.edu>
2024-04-14 11:19:39 -0700

    created RecipeLogDatabase.java

---
+++ app/src/main/java/com/example/recipeshare/Database/RecipeLogDatabase.java
@@ -0,0 +1,5 @@
+package com.example.recipeshare.Database;
+
+public class RecipeLogDatabase {
+    //Refer to Gymlog video 3 @ 16 min
+}
--- app/src/main/java/com/example/recipeshare/Database/entities/RecipeLog.java
+++ app/src/main/java/com/example/recipeshare/Database/entities/RecipeLog.java
@@ -1,4 +1,5 @@
 package com.example.recipeshare.Database.entities;
 
 public class RecipeLog {
+    //Refer to GymLog video 3 @ 10 min
 }
