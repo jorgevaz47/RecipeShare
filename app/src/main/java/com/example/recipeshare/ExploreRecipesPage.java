d88f6e9f237d03034353ace70c8f7f8a66c407d4
Jesus9498 <jmartinezmiranda@csumb.edu>
2024-04-13 19:20:32 -0700

    created ExploreRecipesPage.java

---
+++ app/src/main/java/com/example/recipeshare/ExploreRecipesPage.java
@@ -0,0 +1,14 @@
+package com.example.recipeshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExploreRecipesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_recipes_page);
+    }
+}
