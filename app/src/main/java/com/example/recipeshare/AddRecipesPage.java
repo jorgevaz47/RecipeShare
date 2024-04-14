f6c024d92e6140a1a86892f9f223bedd3a401797
Jesus9498 <jmartinezmiranda@csumb.edu>
2024-04-13 19:44:24 -0700

    Created AddRecipesPage.java and filled with title and button. Button not linked.

---
+++ app/src/main/java/com/example/recipeshare/AddRecipesPage.java
@@ -0,0 +1,14 @@
+package com.example.recipeshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRecipesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipes_page);
+    }
+}
