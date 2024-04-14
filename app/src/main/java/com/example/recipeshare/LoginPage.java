ea9b88fc3ade66d94fdb3100c930efc8f5c9ed79
Jesus9498 <jmartinezmiranda@csumb.edu>
2024-04-14 10:46:15 -0700

    Removed .kts from gradle files and adjusted them to normal. created binding and onClick in LoginPage and more

--- app/src/main/java/com/example/recipeshare/LoginPage.java
+++ app/src/main/java/com/example/recipeshare/LoginPage.java
@@ -3,12 +3,27 @@ package com.example.recipeshare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.recipeshare.databinding.ActivityLoginPageBinding;
 
 public class LoginPage extends AppCompatActivity {
+
+    ActivityLoginPageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        binding = ActivityLoginPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginPage.this, "Working", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
