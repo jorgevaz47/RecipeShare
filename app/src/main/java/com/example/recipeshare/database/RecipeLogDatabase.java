f6765fa72cab4fcbbc494a0e3cd400b3fca01dc3
Jesus9498 <jmartinezmiranda@csumb.edu>
2024-04-14 15:28:32 -0700

    Created User.java and UserDAO.java

--- app/src/main/java/com/example/recipeshare/database/RecipeLogDatabase.java
+++ app/src/main/java/com/example/recipeshare/database/RecipeLogDatabase.java
@@ -1,5 +1,14 @@
 package com.example.recipeshare.database;
 
+import androidx.room.Database;
+
+import com.example.recipeshare.database.entities.RecipeLog;
+import com.example.recipeshare.database.entities.Users;
+
+@Database(entities = {RecipeLog.class, Users.class}, version = 2, exportSchema = false)
+
 public class RecipeLogDatabase {
+    public static final String USER_TABLE = "user_table";
+
     //Refer to Gymlog video 3 @ 16 min
 }
---
+++ app/src/main/java/com/example/recipeshare/database/UserDAO.java
@@ -0,0 +1,4 @@
+package com.example.recipeshare.database;
+
+public interface UserDAO {
+}
