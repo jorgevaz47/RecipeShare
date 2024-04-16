6087aa29589eae587f663afb454a41f722504ba6
jorgevaz47 <jav200021@gmail.com>
2024-04-16 11:45:57 -0700

    Moved some of the login functionality from the ExploreRecipes page to the MainActivity page

--- app/src/main/java/com/example/recipeshare/ExploreRecipesPage.java
+++ app/src/main/java/com/example/recipeshare/ExploreRecipesPage.java
@@ -10,32 +10,16 @@ import com.example.recipeshare.databinding.ActivityMainBinding;
 
 public class ExploreRecipesPage extends AppCompatActivity {
 
-    private static final String EXPLORE_RECIPES_PAGE_USER_ID = "com.example.recipeshare.EXPLORE_RECIPES_PAGE_USER_ID";
+
     ActivityMainBinding binding;
 
-    int loggedUserID = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        loginUser();
-
-        if(loggedUserID == -1){
-            Intent intent = LoginPage.loginIntentFactory(getApplicationContext());
-            startActivity(intent);
-        }
     }
 
-    private void loginUser() {
-        loggedUserID = getIntent().getIntExtra(EXPLORE_RECIPES_PAGE_USER_ID, -1);
-    }
 
-    static Intent exploreRecipesPageIntentFactory(Context context, int userID){
-        Intent intent = new Intent(context, ExploreRecipesPage.class);
-        intent.putExtra(EXPLORE_RECIPES_PAGE_USER_ID, userID);
-        return intent;
-    }
 }
--- app/src/main/java/com/example/recipeshare/MainActivity.java
+++ app/src/main/java/com/example/recipeshare/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.recipeshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.recipeshare.database.RecipeLogRepository;
@@ -12,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     private RecipeLogRepository repository;
-
+    private static final String MAIN_ACTIVITY_PAGE_USER_ID = "com.example.recipeshare.MAIN_ACTIVITY_PAGE_USER_ID";
     public static final String TAG = "DAC_RECIPELOG";
     String mName = "";
     String mIngredients = "";
@@ -21,8 +23,7 @@ public class MainActivity extends AppCompatActivity {
     boolean mIsFavorite = false;
 
     //TODO: Add login information.. refer video 7 @ 22min
-    int loggedInUserId = -1;
-
+    int loggedUserID = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +31,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         repository = RecipeLogRepository.getRepository(getApplication());
+
+        loginUser();
+
+        if(loggedUserID == -1){
+            Intent intent = LoginPage.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
         //todo: working on video 5, wondering if all this data should be on main or diff class
     }
 
     //TODO: should this be on main? we are not displaying recipes here...Display should be on explore, myRecipes, favRecipes. video 6
     private void insertRecipeLogRecords(){
-        RecipeLog log = new RecipeLog(mName, mIngredients, mInstructions, mCreatedBy, mIsFavorite, loggedInUserId);
+        RecipeLog log = new RecipeLog(mName, mIngredients, mInstructions, mCreatedBy, mIsFavorite, loggedUserID);
         repository.insertRecipeLog(log);
+    }
+
+    private void loginUser() {
+        loggedUserID = getIntent().getIntExtra(MAIN_ACTIVITY_PAGE_USER_ID, -1);
+    }
+
+    static Intent mainActivityPageIntentFactory(Context context, int userID){
+        Intent intent = new Intent(context, ExploreRecipesPage.class);
+        intent.putExtra(MAIN_ACTIVITY_PAGE_USER_ID, userID);
+        return intent;
     }
 }
