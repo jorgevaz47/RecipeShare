27dfb1fb22357f6f5f90d1ca88bf2cca8276a3d8
Jesus9498 <jmartinezmiranda@csumb.edu>
2024-04-16 18:52:07 -0700

    functioning back buttons in most if not all pages

--- app/src/main/java/com/example/recipeshare/FavoriteRecipesPage.java
+++ app/src/main/java/com/example/recipeshare/FavoriteRecipesPage.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FavoriteRecipesPage extends AppCompatActivity {
 
+    Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_recipes_page);
+
+        backButton = findViewById(R.id.backButtonFavRecipes);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
     static Intent favoriteRecipesIntentFactory(Context context){
--- app/src/main/java/com/example/recipeshare/MyRecipes.java
+++ app/src/main/java/com/example/recipeshare/MyRecipes.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyRecipes extends AppCompatActivity {
 
+    Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_recipes);
+
+        backButton = findViewById(R.id.backButtonMyRecipes);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
     static Intent myRecipesIntentFactory(Context context){
