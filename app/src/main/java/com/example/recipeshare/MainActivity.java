f6f1348755964e4560e2f389e7fbe600cfa74e2e
Jesus9498 <jmartinezmiranda@csumb.edu>
2024-04-18 16:42:39 -0700

    admin button functioning

--- app/src/main/java/com/example/recipeshare/MainActivity.java
+++ app/src/main/java/com/example/recipeshare/MainActivity.java
@@ -97,6 +97,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        binding.adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = AdminPage.adminIntentFactory(MainActivity.this);
+                startActivity(intent);
+            }
+        });
     }
 
 
